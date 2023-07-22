1def32d427e32deedd91ee9b53c0ecbdf47393e4
Kyle Biondich <kyle.biondich@boeing.com>
2023-07-22 12:21:00 -0700

    Started hw 3

---
+++ assignments/_PythonClass/Assignment03/HomeInventory.py
@@ -0,0 +1,7 @@
+# ----------------------------------------------- #
+# Title: Basic Math Script
+# Dev: KBiondich 
+# Desc: Script that captures two numbers from the user, performs calculations on those numbers, and returns the results of the calculations.
+# Changelog: (who, when, what)
+# 	kbiondich,7/14/23,Created File
+# ------------------------------------------------#
