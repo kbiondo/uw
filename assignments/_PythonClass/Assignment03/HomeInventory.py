63bb56462912c91a4260dea009e219c154e442c2
Kyle Biondich <kyle.biondich@boeing.com>
2023-07-28 08:15:22 -0700

    finished hw3

--- assignments/_PythonClass/Assignment03/HomeInventory.py
+++ assignments/_PythonClass/Assignment03/HomeInventory.py
@@ -1,7 +1,28 @@
 # ----------------------------------------------- #
-# Title: Basic Math Script
+# Title: Home Inventory Script
 # Dev: KBiondich 
-# Desc: Script that captures two numbers from the user, performs calculations on those numbers, and returns the results of the calculations.
+# Desc: A script that captures a household item, its estimated value and stores it in a txt file
 # Changelog: (who, when, what)
-# 	kbiondich,7/14/23,Created File
+# 	kbiondich,7/22/23,Created File
 # ------------------------------------------------#
+
+# pseudo-funs
+# Get user input
+# Save the input to a file
+# Display the results
+
+# Create an object and call the open function and pass the "a" argument to indicate appending information to the file
+objFile = open("Inventory.txt", "a")
+
+# Inform the user how to exit the while loop 
+print("Type in a string to write (Enter 'Exit' to quit!)")
+
+# Begin a while loop that captures user input, checks for the exit condition, 
+# and writes the info to the file if the condition evaluates to false 
+while(True):
+  houseItem = input("Enter a household item: ")
+  if(houseItem.lower() == "exit"): break
+  itemValue = input(f"Enter {houseItem} value: ")
+  if(itemValue.lower() == "exit"): break
+  else: objFile.write("Item: " + houseItem + " Value: " + itemValue + "\n")
+objFile.close()
