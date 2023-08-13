d085318dffda117319a64b7589f8a482598e6c24
Kyle <626100+kbiondo@users.noreply.github.com>
2023-08-13 12:01:17 -0700

    finished hw 06 python

--- assignments/_PythonClass/Assignment06/Assignment06.py
+++ assignments/_PythonClass/Assignment06/Assignment06.py
@@ -32,13 +32,16 @@ class Processor:
             list: list of dictionary rows
         """        
         # Reads data from a file into a list of dictionary rows
+        
         file_obj = open(file_name, "a")
+
         list_of_rows.clear()  # clear current data
+
         file_obj = open(file_name, "r")
+
         for row in file_obj:
             file_data = row.split(",")
-            row_dic = {"Task": file_data[0].strip(), "Priority": file_data[1].strip()}
-            list_of_rows = list_of_rows.append(row_dic)
+            list_of_rows.append({"Task": file_data[0].strip(), "Priority": file_data[1].strip()})
         return list_of_rows
     
     @staticmethod
@@ -51,13 +54,16 @@ class Processor:
         :return: (list) of dictionary rows
         """
         row_dic = {"Task": task.strip(), "Priority": priority.strip()}
-        for items in list_of_rows:
-            if items["Task"] == task:
+ 
+        items = 0 # start counter at 0
+        while items < len(list_of_rows):
+            if list_of_rows[items]["Task"] != task:
+                items += 1
+            elif list_of_rows[items]["Task"] == task:
                 return list_of_rows
-            else:
-                list_of_rows.append(row_dic)
+        list_of_rows.append(row_dic)
         return list_of_rows
-
+    
     @staticmethod
     def remove_data_from_list(task, list_of_rows):
         """ Removes data from a list of dictionary rows
@@ -67,13 +73,18 @@ class Processor:
         :return: (list) of dictionary rows
         """
         # TODO: Add Code Here!
-        for items in list_of_rows:
-            if items['Task'] == task:
-                list_of_rows.remove(items)
+
+        items = 0 # start counter at 0
+        while items < len(list_of_rows):
+            if list_of_rows[items]["Task"] != task:
+                items += 1
+            elif list_of_rows[items]["Task"] == task:
+                list_of_rows.pop(items)
             else:
-                continue
+                break
         return list_of_rows
 
+    
     @staticmethod
     def write_data_to_file(file_name, list_of_rows):
         """ Writes data from a list of dictionary rows to a File
@@ -83,13 +94,12 @@ class Processor:
         :return: (list) of dictionary rows
         """
         # TODO: Add Code Here!
-        write_file = open(file_name, "a")
+        write_file = open(file_name, "w")
         for items in list_of_rows:
-            write_file.write(items['task'] + ',' + items['priority'])
+            write_file.write(items['Task'] + ',' + items['Priority'] + '\n')
         write_file.close()
 
         return list_of_rows
-
 
 # Presentation (Input/Output)  -------------------------------------------- #
 
@@ -118,10 +128,11 @@ class IO:
 
         :return: string
         """
+
         choice = str(input("Which option would you like to perform? [1 to 4] - ")).strip()
         print()  # Add an extra line for looks
         return choice
-
+    
     @staticmethod
     def output_current_tasks_in_list(list_of_rows):
         """ Shows the current Tasks in the list of dictionaries rows
@@ -158,14 +169,11 @@ class IO:
         return findTask
 
 
-
 # Main Body of Script  ------------------------------------------------------ #
-
 
 # Step 1 - When the program starts, Load data from ToDoFile.txt.
 Processor.read_data_from_file( file_name=file_name_str, list_of_rows=table_lst)  # read file data
 
-# Step 2 - Display a menu of choices to the user
 while (True):
     # Step 3 Show current data
     IO.output_current_tasks_in_list(list_of_rows=table_lst)  # Show current data in the list/table
