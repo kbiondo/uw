911261a73fd51441762ecb30ee8a8715c8e95b3e
Kyle Biondich <626100+kbiondo@users.noreply.github.com>
2023-08-18 08:36:05 -0700

    added submodule 07

---
+++ assignments/_PythonClass/Assignment07/Assignment07_KyleBiondich.py
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------------------------- #
+# Title: Assignment 07
+# Description: Working with classes in a module,
+# ChangeLog (Who,When,What):
+# KBiondich,8-18-2023,Modified code to complete assignment 07
+# ---------------------------------------------------------------------------- #
+
